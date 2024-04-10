b1ef9116357758230b5e4d0a5d283227a264d756
Siddhesh Bhande <63193665+Siddhesh-Bhande@users.noreply.github.com>
2024-04-10 00:37:49 -0400

    Update DatePickerComp.js
    Bug Fixed. Application crashed if the user selected invalid dates. This will prevent crash and show available data within the date range.

--- frontend/src/Components/Visualisations/visualisation_utilities/DatePickerComp.js
+++ frontend/src/Components/Visualisations/visualisation_utilities/DatePickerComp.js
@@ -11,7 +11,7 @@ export default function DatePickerComp({ min, max, setSelectedFilters }) {
       key: "selection",
     },
   ]);
-  // Function to format date to 'yyyy-mm-dd'
+  //Function to format date to 'yyyy-mm-dd'
   const formatDate = (date) => {
     const yyyy = date.getFullYear();
     const mm = String(date.getMonth() + 1).padStart(2, "0"); // January is 0!
@@ -19,27 +19,40 @@ export default function DatePickerComp({ min, max, setSelectedFilters }) {
     return `${yyyy}-${mm}-${dd}`;
   };
 
-  function dateChangeHandler(item) {
-    console.log([item.selection]);
-    setState([item.selection]);
-    const { startDate, endDate } = item.selection;
+  // function dateChangeHandler(item) {
+  //   console.log([item.selection]);
+  //   setState([item.selection]);
+  //   const { startDate, endDate } = item.selection;
+  //   setSelectedFilters((prevState) => ({
+  //     ...prevState,
+  //     dates: {
+  //       startDate: formatDate(startDate),
+  //       endDate: formatDate(endDate),
+  //     },
+  //   }));
+  // }
+
+  function tempdateChangeHandler(item) {
+    console.log("this is sd" + formatDate(item.selection.startDate));
+    console.log("this is ed" + formatDate(item.selection.endDate));
     setSelectedFilters((prevState) => ({
       ...prevState,
       dates: {
-        startDate: formatDate(startDate),
-        endDate: formatDate(endDate),
+        startDate: formatDate(item.selection.startDate),
+        endDate: formatDate(item.selection.endDate),
       },
     }));
+    setState([item.selection]);
   }
 
   return (
     <DateRangePicker
       // onChange={(item) => setState([item.selection])}
-      onChange={(item) => dateChangeHandler(item)}
+      onChange={(item) => tempdateChangeHandler(item)}
       showSelectionPreview={true}
       moveRangeOnFirstSelection={false}
-      minDate={min}
-      maxDate={max}
+      // minDate={min}
+      // maxDate={max}
       months={1}
       ranges={state}
       direction="horizontal"
